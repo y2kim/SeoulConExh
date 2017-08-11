522f527188f261c04c0fd397cc27b56b0aa97677
y2kim <kchy0806@naver.com>
2017-08-11 19:31:32 +0900

    2nd job
    다른 작업땜시  연결 및 인트로 빈 액티비티만 생성

--- Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
+++ Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
@@ -2,8 +2,20 @@ package com.seoulconexh.kcy.seoul_traffic_info;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import kr.go.seoul.trafficbus.TrafficBusButtonTypeB;
+import kr.go.seoul.trafficbus.TrafficBusButtonTypeN;
+import kr.go.seoul.trafficsubway.TrafficSubwayButtonTypeA;
+import kr.go.seoul.airquality.AirQualityTypeMini;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String key = "5a6c4f63676b636836384858566346";
+
+    private TrafficBusButtonTypeB busTypeB;
+    private TrafficBusButtonTypeN busTypeN;
+    private TrafficSubwayButtonTypeA subwayButtonTypeA;
+    private AirQualityTypeMini typeMini;
+// AAR 를 사용  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
