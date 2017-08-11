3361ba1cafb30e14642a612a295b9aae1273a5da
y2kim <kchy0806@naver.com>
2017-08-11 18:59:30 +0900

    Creat Project
    생성만

---
+++ MyApplication/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
@@ -0,0 +1,13 @@
+package com.seoulconexh.kcy.seoul_traffic_info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
