d903d90bea7286e65be73a6e492697a7a5717aba
y2kim <kchy0806@naver.com>
2017-08-13 14:39:36 +0900

    5번째 작업 변경및 중단
    원래 지하철을 TYPE A 를 쓸려고 했으나 실시간을 지원하지 않았고 버스 데이터도 새로 다른 API키를 이용해야함 인증 허가 날때까지 중단  하지만 디자인 구상은 계속

--- Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
+++ Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
@@ -6,14 +6,17 @@ import kr.go.seoul.trafficbus.TrafficBusButtonTypeB;
 import kr.go.seoul.trafficbus.TrafficBusButtonTypeN;
 import kr.go.seoul.trafficsubway.TrafficSubwayButtonTypeA;
 import kr.go.seoul.airquality.AirQualityTypeMini;
+import kr.go.seoul.trafficsubway.TrafficSubwayButtonTypeB;
 
 public class MainActivity extends AppCompatActivity {
 
     private String key = "5a6c4f63676b636836384858566346";
+    private String subwayKey = "sample"; // 지하철 실시간 시간
 
     private TrafficBusButtonTypeB busTypeB;
     private TrafficBusButtonTypeN busTypeN;
     private TrafficSubwayButtonTypeA subwayButtonTypeA;
+    private TrafficSubwayButtonTypeB subwayButtonTypeB;
     private AirQualityTypeMini typeMini;
 // AAR 를 사용
 
