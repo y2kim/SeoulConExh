e90804b537aa1c3318e2f7b9049dcacb2436b6c2
y2kim <kchy0806@naver.com>
2017-08-14 18:44:55 +0900

    7th 현재 버스 때문에 막힘
    아마 16일정도 기능 재개 가능할것 같음  여의치 않으면버스를 뺄수도 있음

--- Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
+++ Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
@@ -12,13 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     private String key = "5a6c4f63676b636836384858566346";
     private String subwayKey = "4971434c466b63683539686464636c"; // 지하철 실시간 시간
+    private String busKey = "62%2FsfGqNoWk%2BFSOxNXLyhw%2B7HUV%2FCJJ41P1nFSxUJ8ukbYYIOzQ7GZMrgVbnp6mPQ9OjMQde0EB%2FfrSpWP%2F16w%3D%3D";
 
     private TrafficBusButtonTypeB busTypeB;
     private TrafficBusButtonTypeN busTypeN;
     private TrafficSubwayButtonTypeA subwayButtonTypeA;
     private TrafficSubwayButtonTypeB subwayButtonTypeB;
     private AirQualityTypeMini typeMini;
-// AAR 를 사용
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         typeMini = (AirQualityTypeMini) findViewById(R.id.type_mini);
-
         subwayButtonTypeB = (TrafficSubwayButtonTypeB) findViewById(R.id.subway_type_b);
+        busTypeB = (TrafficBusButtonTypeB) findViewById(R.id.bus_type_b);
 
         typeMini.setOpenAPIKey(key);
         subwayButtonTypeB.setOpenAPIKey(key);
-        subwayButtonTypeB.setsubwayLocationAPIKey(subwayKey);
+        subwayButtonTypeB.setsubwayLocationAPIKey(subwayKey); // 실시간 도착
+        busTypeB.setOpenAPIKey(busKey);
     }
 }
