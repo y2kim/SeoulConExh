a38ee7bd5226b9e0ad3c7b215eb1661ab4c84740
ytn <hotflylm@gmail.com>
2017-08-16 15:25:05 +0900

    디자인 구성 및 다듬기
    내일 집 컴에도 테스트  현재 학교 컴

--- Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
+++ Seoul_Traffic_Info/app/src/main/java/com/seoulconexh/kcy/seoul_traffic_info/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import kr.go.seoul.trafficbus.TrafficBusButtonTypeB;
 import kr.go.seoul.trafficbus.TrafficBusButtonTypeN;
-import kr.go.seoul.trafficsubway.TrafficSubwayButtonTypeA;
 import kr.go.seoul.airquality.AirQualityTypeMini;
 import kr.go.seoul.trafficsubway.TrafficSubwayButtonTypeB;
 
@@ -12,11 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     private String key = "5a6c4f63676b636836384858566346";
     private String subwayKey = "4971434c466b63683539686464636c"; // 지하철 실시간 시간
-    private String busKey = "62%2FsfGqNoWk%2BFSOxNXLyhw%2B7HUV%2FCJJ41P1nFSxUJ8ukbYYIOzQ7GZMrgVbnp6mPQ9OjMQde0EB%2FfrSpWP%2F16w%3D%3D";
+    private String busKey = "4971434c466b63683539686464636c";
 
     private TrafficBusButtonTypeB busTypeB;
     private TrafficBusButtonTypeN busTypeN;
-    private TrafficSubwayButtonTypeA subwayButtonTypeA;
     private TrafficSubwayButtonTypeB subwayButtonTypeB;
     private AirQualityTypeMini typeMini;
 
@@ -28,10 +26,14 @@ public class MainActivity extends AppCompatActivity {
         typeMini = (AirQualityTypeMini) findViewById(R.id.type_mini);
         subwayButtonTypeB = (TrafficSubwayButtonTypeB) findViewById(R.id.subway_type_b);
         busTypeB = (TrafficBusButtonTypeB) findViewById(R.id.bus_type_b);
+        busTypeN = (TrafficBusButtonTypeN) findViewById(R.id.bus_type_n);
 
         typeMini.setOpenAPIKey(key);
         subwayButtonTypeB.setOpenAPIKey(key);
         subwayButtonTypeB.setsubwayLocationAPIKey(subwayKey); // 실시간 도착
         busTypeB.setOpenAPIKey(busKey);
+        busTypeN.setOpenAPIKey(busKey);
+
+
     }
 }
